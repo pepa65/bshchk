5c8f1b5b765f0207bb34dacdca92c6de308c3d86
gitlab.com/pepa65 <pepa65@passchier.net>
2025-08-17 22:39:25 +0700

    Fix main

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 const name = "bshchk"
-const version = "0.2.2"
+const version = "0.2.3"
 
 type args struct {
 	Source        string `arg:"positional" help:"If given as '-' or '': read from stdin" default:""`
